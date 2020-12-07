3bd8c520879526ac9eddc7323bc8192598c53a39
Jackson Speedy <speedyj1@rose-hulman.edu>
2020-12-07 12:48:33 -0500

    Initial Commit

---
+++ Flexible Layout Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flexible Layout Practice
+//
+//  Created by Jack Speedy on 12/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
